97c725abeac30d37e32da43a92fc5280feb52248
shabana1990 <shabanak.1990@gmail.com>
2021-06-05 10:39:43 +0530

    Added WPF MVVM Project.
    Caliburn Micro Framework.

---
+++ RMWPFUI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMWPFUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
